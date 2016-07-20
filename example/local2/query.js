1dc615262bbccb44f32c658670d1a135ad6abe22
qiongtubao <810776886@qq.com>
2016-07-20 09:12:57 +0800

    delete query

--- example/local2/query.js
+++ example/local2/query.js
@@ -4,18 +4,15 @@ Mongodb.bindDb("news", config);
 Mongodb.news.command(function(err, client, dbcb) {
 	var time = new Date(2016, 6, 5);
 	
-	client.news.drop(function(err ,result) {
-		console.log(err, result);
-	});
 	
-	/**
-	client.news.find({
+	
+		client.news.find({
 		time: {
 			$gt: time.getTime()
 		}
 	}).toArray(function(err, data) {
 		console.log(err, data);
 	});
-*/
+
 	
 });	
